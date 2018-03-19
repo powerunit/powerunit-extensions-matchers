d84d1c39076e232fbe94aee33980346ba9eac96f
boretti <mathieu.boretti@gmail.com>
2018-03-19 18:26:25 +0100

    #14 reorganize code

--- powerunit-extensions-matchers/src/main/java/ch/powerunit/extensions/matchers/provideprocessor/FieldDescription.java
+++ powerunit-extensions-matchers/src/main/java/ch/powerunit/extensions/matchers/provideprocessor/FieldDescription.java
@@ -19,10 +19,20 @@
  */
 package ch.powerunit.extensions.matchers.provideprocessor;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FieldDescription {
 
 	public static enum Type {
 		NA, ARRAY, COLLECTION, LIST, SET, OPTIONAL, COMPARABLE
+	}
+
+	@FunctionalInterface
+	public interface Generator {
+		String generate(String inputClassName, String returnMethod, String prefix);
 	}
 
 	private final String fieldAccessor;
@@ -30,6 +40,8 @@ public class FieldDescription {
 	private final String methodFieldName;
 	private final String fieldType;
 	private final Type type;
+	private final List<Generator> implGenerator;
+	private final List<Generator> dslGenerator;
 
 	public FieldDescription(String fieldAccessor, String fieldName, String methodFieldName, String fieldType,
 			Type type) {
@@ -38,6 +50,271 @@ public class FieldDescription {
 		this.methodFieldName = methodFieldName;
 		this.fieldType = fieldType;
 		this.type = type;
+		List<Generator> tmp = new ArrayList<>();
+		tmp.add(this::getImplementationForDefault);
+		if (type == Type.ARRAY) {
+			tmp.add(this::getImplementationForArray);
+		}
+		if (type == Type.OPTIONAL) {
+			tmp.add(this::getImplementationForOptional);
+		}
+		if (type == Type.COMPARABLE) {
+			tmp.add(this::getImplementationForComparable);
+		}
+		implGenerator = Collections.unmodifiableList(tmp);
+		tmp = new ArrayList<>();
+		tmp.add(this::getDslForDefault);
+		if (type == Type.ARRAY) {
+			tmp.add(this::getDslForArray);
+		}
+		if (type == Type.OPTIONAL) {
+			tmp.add(this::getDslForOptional);
+		}
+		if (type == Type.COMPARABLE) {
+			tmp.add(this::getDslForComparable);
+		}
+		dslGenerator = Collections.unmodifiableList(tmp);
+	}
+
+	private String getImplementationForDefault(String inputClassName, String returnMethod, String prefix) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "(org.hamcrest.Matcher<? super "
+				+ fieldType + "> matcher) {").append("\n");
+		sb.append(prefix).append("  " + fieldName + "= new " + methodFieldName + "Matcher(matcher);").append("\n");
+		sb.append(prefix).append("  return this;").append("\n");
+		sb.append(prefix).append("}").append("\n");
+
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.is(value));").append("\n");
+		sb.append(prefix).append("}").append("\n");
+
+		return sb.toString();
+	}
+
+	private String getImplementationForArray(String inputClassName, String returnMethod, String prefix) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "IsEmpty() {").append("\n");
+		sb.append(prefix)
+				.append("  return " + fieldName + "((org.hamcrest.Matcher)org.hamcrest.Matchers.emptyArray());")
+				.append("\n");
+		sb.append(prefix).append("}").append("\n");
+		return sb.toString();
+	}
+
+	private String getImplementationForOptional(String inputClassName, String returnMethod, String prefix) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "IsPresent() {").append("\n");
+		sb.append(prefix).append("  " + fieldName + " = " + methodFieldName + "Matcher.isPresent();").append("\n");
+		sb.append(prefix).append("  return this;").append("\n");
+		sb.append(prefix).append("}").append("\n");
+
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "IsNotPresent() {").append("\n");
+		sb.append(prefix).append("  " + fieldName + " = " + methodFieldName + "Matcher.isNotPresent();").append("\n");
+		sb.append(prefix).append("  return this;").append("\n");
+		sb.append(prefix).append("}").append("\n");
+		return sb.toString();
+	}
+
+	private String getImplementationForComparable(String inputClassName, String returnMethod, String prefix) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix)
+				.append("public " + returnMethod + " " + fieldName + "ComparesEqualTo(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.comparesEqualTo(value));")
+				.append("\n");
+		sb.append(prefix).append("}").append("\n");
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "LessThan(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.lessThan(value));").append("\n");
+		sb.append(prefix).append("}").append("\n");
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix)
+				.append("public " + returnMethod + " " + fieldName + "LessThanOrEqualTo(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.lessThanOrEqualTo(value));")
+				.append("\n");
+		sb.append(prefix).append("}").append("\n");
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix).append("public " + returnMethod + " " + fieldName + "GreaterThan(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.greaterThan(value));").append("\n");
+		sb.append(prefix).append("}").append("\n");
+		sb.append(prefix).append("@Override").append("\n");
+		sb.append(prefix)
+				.append("public " + returnMethod + " " + fieldName + "GreaterThanOrEqualTo(" + fieldType + " value) {")
+				.append("\n");
+		sb.append(prefix).append("  return " + fieldName + "(org.hamcrest.Matchers.greaterThanOrEqualTo(value));")
+				.append("\n");
+		sb.append(prefix).append("}").append("\n");
+		return sb.toString();
+	}
+
+	public String getImplementationInterface(String inputClassName, String returnMethod, String prefix) {
+		return implGenerator.stream().map(g -> g.generate(inputClassName, returnMethod, prefix))
+				.collect(Collectors.joining("\n"));
+	}
+
+	public String getDslForDefault(String inputClassName, String returnMethod, String prefix) {
+		String linkToAccessor = "{@link " + inputClassName + "#" + getFieldAccessor()
+				+ " This field is accessed by using this approach}.";
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName + ".").append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param matcher a Matcher on the field.").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName)
+				.append("(org.hamcrest.Matcher<? super " + fieldType + "> matcher);").append("\n");
+
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName + ".").append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix)
+				.append(" * @param value an expected value for the field, which will be compared using the is matcher.")
+				.append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#is(java.lang.Object)").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("(" + fieldType + " value);").append("\n");
+
+		return sb.toString();
+	}
+
+	public String getDslForArray(String inputClassName, String returnMethod, String prefix) {
+		String linkToAccessor = "{@link " + inputClassName + "#" + getFieldAccessor()
+				+ " This field is accessed by using this approach}.";
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName + " that the array is empty.")
+				.append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("IsEmpty();").append("\n");
+
+		return sb.toString();
+	}
+
+	public String getDslForOptional(String inputClassName, String returnMethod, String prefix) {
+		String linkToAccessor = "{@link " + inputClassName + "#" + getFieldAccessor()
+				+ " This field is accessed by using this approach}.";
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName + " with a present optional.")
+				.append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("IsPresent();").append("\n");
+
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName + " with a not present optional.")
+				.append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("IsNotPresent();").append("\n");
+
+		return sb.toString();
+	}
+
+	public String getDslForComparable(String inputClassName, String returnMethod, String prefix) {
+		String linkToAccessor = "{@link " + inputClassName + "#" + getFieldAccessor()
+				+ " This field is accessed by using this approach}.";
+		StringBuilder sb = new StringBuilder();
+		sb.append(prefix).append(" /**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName
+				+ " that this field is equals to another one, using the compareTo method.").append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param value the value to compare with").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#comparesEqualTo(java.lang.Comparable) ").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("ComparesEqualTo(" + fieldType + " value);")
+				.append("\n");
+
+		sb.append(prefix).append(" /**").append("\n");
+		sb.append(prefix).append(
+				" * Add a validation on the field " + fieldName + " that this field is less than another value.")
+				.append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param value the value to compare with").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#lessThan(java.lang.Comparable) ").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("LessThan(" + fieldType + " value);")
+				.append("\n");
+
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName
+				+ " that this field is less or equal than another value.").append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param value the value to compare with").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#lessThanOrEqualTo(java.lang.Comparable) ").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("LessThanOrEqualTo(" + fieldType + " value);")
+				.append("\n");
+
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(
+				" * Add a validation on the field " + fieldName + " that this field is greater than another value.")
+				.append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param value the value to compare with").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#greaterThan(java.lang.Comparable) ").append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName).append("GreaterThan(" + fieldType + " value);")
+				.append("\n");
+
+		sb.append(prefix).append("/**").append("\n");
+		sb.append(prefix).append(" * Add a validation on the field " + fieldName
+				+ " that this field is greater or equal than another value.").append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * ").append(linkToAccessor).append("\n");
+		sb.append(prefix).append(" *").append("\n");
+		sb.append(prefix).append(" * @param value the value to compare with").append("\n");
+		sb.append(prefix).append(" * @return the DSL to continue the construction of the matcher.").append("\n");
+		sb.append(prefix).append(" * @see org.hamcrest.Matchers#greaterThanOrEqualTo(java.lang.Comparable) ")
+				.append("\n");
+		sb.append(prefix).append(" */").append("\n");
+		sb.append(prefix).append(returnMethod).append(fieldName)
+				.append("GreaterThanOrEqualTo(" + fieldType + " value);").append("\n");
+
+		return sb.toString();
+	}
+
+	public String getDslInterface(String inputClassName, String returnMethod, String prefix) {
+		return dslGenerator.stream().map(g -> g.generate(inputClassName, returnMethod, prefix))
+				.collect(Collectors.joining("\n"));
 	}
 
 	public String getFieldAccessor() {
