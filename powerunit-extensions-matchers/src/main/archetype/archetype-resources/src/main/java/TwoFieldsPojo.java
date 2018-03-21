83ad899946691342d0276c040d0041cd93e6a824
boretti <mathieu.boretti@gmail.com>
2018-03-22 00:01:07 +0100

    #20 Add more java basic to the archetype

---
+++ powerunit-extensions-matchers/src/main/archetype/archetype-resources/src/main/java/TwoFieldsPojo.java
@@ -0,0 +1,10 @@
+package \${package};
+
+import ch.powerunit.extensions.matchers.ProvideMatchers;
+
+@ProvideMatchers
+public class TwoFieldsPojo {
+	public String firstField;
+	
+	public String secondField;
+}
