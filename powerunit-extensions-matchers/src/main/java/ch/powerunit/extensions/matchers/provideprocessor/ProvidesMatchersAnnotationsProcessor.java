954b90015b6eaea89cb12edd329c122d12b7ef79
boretti <mathieu.boretti@gmail.com>
2018-03-26 19:00:14 +0200

    Close #45

--- powerunit-extensions-matchers/src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersAnnotationsProcessor.java
+++ powerunit-extensions-matchers/src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersAnnotationsProcessor.java
@@ -22,7 +22,6 @@ package ch.powerunit.extensions.matchers.provideprocessor;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.lang.annotation.Annotation;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
