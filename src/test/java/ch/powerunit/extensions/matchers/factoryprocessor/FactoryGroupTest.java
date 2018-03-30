e3efb5851fd14b8af595f4e605ac7a9451422b04
boretti <mathieu.boretti@gmail.com>
2018-03-30 21:24:41 +0200

    Add one more test on FactoryGroup

--- src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryGroupTest.java
+++ src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryGroupTest.java
@@ -1,6 +1,15 @@
 package ch.powerunit.extensions.matchers.factoryprocessor;
 
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.annotation.processing.Filer;
+import javax.tools.JavaFileObject;
+
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import ch.powerunit.Rule;
 import ch.powerunit.Test;
@@ -12,8 +21,31 @@ public class FactoryGroupTest implements TestSuite {
 	@Mock
 	private FactoryAnnotationsProcessor factoryAnnotationProcessor;
 
+	@Mock
+	private Filer filer;
+
+	@Mock
+	private JavaFileObject javaFileObject;
+
+	private StringWriter outputStream;
+
+	private void prepareMock() {
+		when(factoryAnnotationProcessor.getFiler()).thenReturn(filer);
+		try {
+			when(filer.createSourceFile(Mockito.eq("target"), Mockito.anyVararg())).thenReturn(javaFileObject);
+		} catch (IOException e) {
+			// ignore
+		}
+		outputStream = new StringWriter();
+		try {
+			when(javaFileObject.openWriter()).thenReturn(outputStream);
+		} catch (IOException e) {
+			// ignore
+		}
+	}
+
 	@Rule
-	public final TestRule rules = mockitoRule();
+	public final TestRule rules = mockitoRule().around(before(this::prepareMock));
 
 	@Test
 	public void testConstructorWithOneEntry() {
@@ -27,6 +59,18 @@ public class FactoryGroupTest implements TestSuite {
 		FactoryGroup underTest = new FactoryGroup(factoryAnnotationProcessor, "a.*,b.*:target");
 		assertThat(underTest.getFullyQualifiedTargetName()).is("target");
 		assertThat(underTest.getAcceptingRegex()).is(arrayContaining("a.*", "b.*"));
+	}
+
+	@Test
+	public void testConstructorWithTwoEntryAndNoTargetMethod() {
+		FactoryGroup underTest = new FactoryGroup(factoryAnnotationProcessor, "a.*,b.*:target");
+		underTest.processGenerateOneFactoryInterface();
+		String target = outputStream.toString().replace('\r', '\n');
+		assertThat(target).is(containsString("package target"));
+		assertThat(target).is(containsString(
+				"@javax.annotation.Generated(value=\"ch.powerunit.extensions.matchers.factoryprocessor.FactoryAnnotationsProcessor\","));
+		assertThat(target).is(containsString("public interface target {"));
+		assertThat(target).is(containsString("public static final target DSL = new target() {}"));
 	}
 
 }
