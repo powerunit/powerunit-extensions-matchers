4dff5e15cc29fffb3b1978872e283024697dec5a
boretti <mathieu.boretti@gmail.com>
2018-04-02 19:02:54 +0200

    Add test to ```ProvidesMatchersSubElementVisitor``` for rejected case.

--- src/test/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitorTest.java
+++ src/test/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitorTest.java
@@ -2,10 +2,18 @@ package ch.powerunit.extensions.matchers.provideprocessor;
 
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
 
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -24,10 +32,19 @@ public class ProvidesMatchersSubElementVisitorTest implements TestSuite {
 	private ProcessingEnvironment processingEnv;
 
 	@Mock
+	private Messager messager;
+
+	@Mock
 	private FieldDescription fieldDescription;
 
 	@Mock
 	private Element targetElement;
+
+	@Mock
+	private VariableElement variableElement;
+
+	@Mock
+	private ExecutableElement executableElement;
 
 	@Rule
 	public final TestRule rules = mockitoRule().around(before(this::prepare));
@@ -35,6 +52,7 @@ public class ProvidesMatchersSubElementVisitorTest implements TestSuite {
 	private void prepare() {
 		underTest = new ProvidesMatchersSubElementVisitor(processingEnv, a -> false);
 		when(fieldDescription.getFieldElement()).thenReturn(targetElement);
+		when(processingEnv.getMessager()).thenReturn(messager);
 	}
 
 	private ProvidesMatchersSubElementVisitor underTest;
@@ -63,5 +81,125 @@ public class ProvidesMatchersSubElementVisitorTest implements TestSuite {
 		Optional<FieldDescription> ofd = underTest.defaultAction(targetElement, providesMatchersAnnotatedElementMirror);
 		assertThat(ofd).isNotNull();
 		assertThat(ofd.isPresent()).is(false);
+	}
+
+	@Test
+	public void testVisitVariableNoPublicAndNotInListThenEmptyAndNoWarning() {
+		when(variableElement.getModifiers()).thenReturn(Collections.emptySet());
+		Optional<FieldDescription> ofd = underTest.visitVariable(variableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager, Mockito.never()).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror, Mockito.never()).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitVariablePublicAndStaticAndNotInListThenEmptyAndNoWarning() {
+		when(variableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC, Modifier.STATIC)));
+		Optional<FieldDescription> ofd = underTest.visitVariable(variableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager, Mockito.never()).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror, Mockito.never()).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutableNoPublicAndNotInListThenEmptyAndNoWarning() {
+		when(executableElement.getModifiers()).thenReturn(Collections.emptySet());
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager, Mockito.never()).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror, Mockito.never()).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutablePublicAndStaticAndNotInListThenEmptyAndNoWarning() {
+		when(executableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC, Modifier.STATIC)));
+		when(executableElement.getParameters()).thenReturn(Collections.emptyList());
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager, Mockito.never()).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror, Mockito.never()).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutablePublicStaticAndNotSize0AndNotInListThenEmptyAndNoWarning() {
+		when(executableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC)));
+		when(executableElement.getParameters()).thenReturn((List) Collections.singletonList(variableElement));
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager, Mockito.never()).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror, Mockito.never()).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitVariableNoPublicAndInListThenEmptyAndWarning() {
+		when(variableElement.getModifiers()).thenReturn(Collections.emptySet());
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		Optional<FieldDescription> ofd = underTest.visitVariable(variableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitVariablePublicAndStaticAndInListThenEmptyAndWarning() {
+		when(variableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC, Modifier.STATIC)));
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		Optional<FieldDescription> ofd = underTest.visitVariable(variableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutableNoPublicAndInListThenEmptyAndWarning() {
+		when(executableElement.getModifiers()).thenReturn(Collections.emptySet());
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutablePublicAndStaticAndInListThenEmptyAndWarning() {
+		when(executableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC, Modifier.STATIC)));
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		when(executableElement.getParameters()).thenReturn(Collections.emptyList());
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror).removeFromIgnoreList(Mockito.any());
+	}
+
+	@Test
+	public void testVisitExecutablePublicStaticAndNotSize0AndInListThenEmptyAndWarning() {
+		when(executableElement.getModifiers()).thenReturn(new HashSet(Arrays.asList(Modifier.PUBLIC)));
+		when(providesMatchersAnnotatedElementMirror.isInsideIgnoreList(Mockito.any())).thenReturn(true);
+		when(executableElement.getParameters()).thenReturn((List) Collections.singletonList(variableElement));
+		Optional<FieldDescription> ofd = underTest.visitExecutable(executableElement,
+				providesMatchersAnnotatedElementMirror);
+		assertThat(ofd).isNotNull();
+		assertThat(ofd.isPresent()).is(false);
+		Mockito.verify(messager).printMessage(Mockito.any(), Mockito.anyString(), Mockito.any());
+		Mockito.verify(providesMatchersAnnotatedElementMirror).removeFromIgnoreList(Mockito.any());
 	}
 }
