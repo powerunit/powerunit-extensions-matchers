8bd68ec8e0167a695f76ba86e6dca8010a209e21
boretti <mathieu.boretti@gmail.com>
2018-04-02 14:39:59 +0200

    Add test on FactoryAnnotationProcessor without element but generation

--- src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryAnnotationProcessorTest.java
+++ src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryAnnotationProcessorTest.java
@@ -2,6 +2,8 @@ package ch.powerunit.extensions.matchers.factoryprocessor;
 
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Collections;
 
 import javax.annotation.processing.Filer;
@@ -11,11 +13,12 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.JavaFileObject;
 import javax.tools.Diagnostic.Kind;
 
-import org.hamcrest.Matcher;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 
 import ch.powerunit.Rule;
 import ch.powerunit.Test;
@@ -45,11 +48,18 @@ public class FactoryAnnotationProcessorTest implements TestSuite {
 	@Mock
 	private TypeElement factoryTE;
 
+	@Mock
+	private JavaFileObject javaFileObject;
+
+	@Spy
+	private StringWriter outputStream = new StringWriter();
+
 	private void prepareMock() {
 		when(processingEnv.getMessager()).thenReturn(messageUtils);
 		when(processingEnv.getFiler()).thenReturn(filer);
 		when(processingEnv.getTypeUtils()).thenReturn(typeUtils);
 		when(processingEnv.getElementUtils()).thenReturn(elements);
+		when(elements.getTypeElement("org.hamcrest.Factory")).thenReturn(factoryTE);
 	}
 
 	@Rule
@@ -83,5 +93,27 @@ public class FactoryAnnotationProcessorTest implements TestSuite {
 		when(processingEnv.getOptions()).thenReturn(Collections.emptyMap());
 		underTest.init(processingEnv);
 		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(false);
+	}
+
+	@Test
+	public void testProcessOneTargetNoAnnotatedElement() throws IOException {
+		when(filer.createSourceFile(Mockito.eq("target"), Mockito.anyVararg())).thenReturn(javaFileObject);
+		when(javaFileObject.openWriter()).thenReturn(outputStream);
+		when(processingEnv.getOptions()).thenReturn(
+				Collections.singletonMap(FactoryAnnotationsProcessor.class.getName() + ".targets", ".*:target"));
+		underTest.init(processingEnv);
+		when(roundEnv.processingOver()).thenReturn(false, true);
+		// First round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+		// Second round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+
+		// Validate
+		String target = outputStream.toString().replace('\r', '\n');
+		assertThat(target).is(containsString("package target"));
+		assertThat(target).is(containsString(
+				"@javax.annotation.Generated(value=\"ch.powerunit.extensions.matchers.factoryprocessor.FactoryAnnotationsProcessor\","));
+		assertThat(target).is(containsString("public interface target {"));
+		assertThat(target).is(containsString("public static final target DSL = new target() {}"));
 	}
 }
