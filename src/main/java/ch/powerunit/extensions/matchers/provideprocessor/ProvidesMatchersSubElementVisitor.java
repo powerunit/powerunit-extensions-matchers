ae59676999599f8f7358d3a528169997880c05c9
Mathieu Boretti <mathieu.boretti@gmail.com>
2018-04-02 20:15:39 +0200

    Integrate preparation for version 0.1.0 (#88)
    * Prepare pom
    
    * Add unit test before working on version 0.1 - Factory part (#85)
    
    * Add test to FactoryGroup
    
    * Cleanup import
    
    * Add test on FactoryAnnotationProcessor without element but generation
    
    * Add test with executable env
    
    * Add more test
    
    * Add unit test to provider code, before start development (#87)
    
    * Prepare test for type extractor
    
    * Remove issue tracking (done by github itself)
    
    * Add test on the NameExtractorVisitor
    
    * Prepare code ```ProvidesMatchersSubElementVisitor``` before testing
    
    * Add first test to ```ProvidesMatchersSubElementVisitor```
    
    * Add test to ```ProvidesMatchersSubElementVisitor``` for rejected case.
    
    * Add test to ```ProvidesMatchersSubElementVisitor``` for ok case
    
    * Add test on visitExecutable
    
    * Fix issue

--- src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitor.java
+++ src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitor.java
@@ -27,13 +27,8 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.ArrayType;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.PrimitiveType;
 import javax.lang.model.type.TypeMirror;
-import javax.lang.model.type.TypeVariable;
 import javax.lang.model.util.SimpleElementVisitor8;
-import javax.lang.model.util.TypeKindVisitor8;
 import javax.tools.Diagnostic.Kind;
 
 /**
@@ -45,89 +40,26 @@ public class ProvidesMatchersSubElementVisitor
 
 	private final ProcessingEnvironment processingEnv;
 	private final Predicate<Element> isInSameRound;
-	private final ExtractNameVisitor extractNameVisitor = new ExtractNameVisitor();
-
-	private final class ExtractNameVisitor extends TypeKindVisitor8<String, Boolean> {
-
-		@Override
-		public String visitPrimitiveAsBoolean(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "boolean" : "Boolean";
-		}
-
-		@Override
-		public String visitPrimitiveAsByte(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "byte" : "Byte";
-		}
-
-		@Override
-		public String visitPrimitiveAsShort(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "short" : "Short";
-		}
-
-		@Override
-		public String visitPrimitiveAsInt(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "int" : "Integer";
-		}
-
-		@Override
-		public String visitPrimitiveAsLong(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "long" : "Long";
-		}
-
-		@Override
-		public String visitPrimitiveAsChar(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "char" : "Character";
-		}
-
-		@Override
-		public String visitPrimitiveAsFloat(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "float" : "Float";
-		}
-
-		@Override
-		public String visitPrimitiveAsDouble(PrimitiveType t, Boolean asPrimitif) {
-			return (asPrimitif) ? "double" : "Double";
-		}
-
-		@Override
-		public String visitArray(ArrayType t, Boolean asPrimitif) {
-			return parseType(t.getComponentType(), true) + "[]";
-		}
-
-		@Override
-		public String visitDeclared(DeclaredType t, Boolean asPrimitif) {
-			return t.toString();
-		}
-
-		@Override
-		public String visitTypeVariable(TypeVariable t, Boolean asPrimitif) {
-			return t.toString();
-		}
-
-		@Override
-		public String visitUnknown(TypeMirror t, Boolean asPrimitif) {
-			processingEnv.getMessager().printMessage(Kind.MANDATORY_WARNING, "Unsupported type element",
-					processingEnv.getTypeUtils().asElement(t));
-			return null;
-		}
-	}
+	private final NameExtractorVisitor extractNameVisitor;
 
 	public ProvidesMatchersSubElementVisitor(ProcessingEnvironment processingEnv, Predicate<Element> isInSameRound) {
 		this.processingEnv = processingEnv;
 		this.isInSameRound = isInSameRound;
+		this.extractNameVisitor = new NameExtractorVisitor(processingEnv);
+	}
+
+	public static Optional<FieldDescription> removeIfNeededAndThenReturn(Optional<FieldDescription> fieldDescription,
+			ProvidesMatchersAnnotatedElementMirror p) {
+		fieldDescription.ifPresent(f -> p.removeFromIgnoreList(f.getFieldElement()));
+		return fieldDescription;
 	}
 
 	@Override
 	public Optional<FieldDescription> visitVariable(VariableElement e, ProvidesMatchersAnnotatedElementMirror p) {
 		if (e.getModifiers().contains(Modifier.PUBLIC) && !e.getModifiers().contains(Modifier.STATIC)) {
 			String fieldName = e.getSimpleName().toString();
-			String fieldType = parseType(e.asType(), false);
-			if (fieldType != null) {
-				p.removeFromIgnoreList(e);
-				return Optional.of(new FieldDescription(p, fieldName, fieldName, fieldType,
-						isInSameRound.test(processingEnv.getTypeUtils().asElement(e.asType())), processingEnv, e,
-						e.asType()));
-			}
+			return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, fieldName, fieldName,
+					e.asType());
 		}
 		if (p.isInsideIgnoreList(e)) {
 			processingEnv.getMessager().printMessage(Kind.MANDATORY_WARNING,
@@ -144,42 +76,42 @@ public class ProvidesMatchersSubElementVisitor
 				&& !e.getModifiers().contains(Modifier.STATIC)) {
 			String simpleName = e.getSimpleName().toString();
 			if (simpleName.startsWith("get")) {
-				return Optional.ofNullable(visiteExecutableGet(e, "get", p));
+				return visiteExecutableGet(e, "get", p);
 			} else if (simpleName.startsWith("is")) {
-				return Optional.ofNullable(visiteExecutableGet(e, "is", p));
+				return visiteExecutableGet(e, "is", p);
 			}
 		}
 		if (p.isInsideIgnoreList(e)) {
 			processingEnv.getMessager().printMessage(Kind.MANDATORY_WARNING,
-					"One of the annotation is not supported as this location ; CHeck that this method is public, doesn't have any parameter and is named isXXX or getXXX",
+					"One of the annotation is not supported as this location ; Check that this method is public, doesn't have any parameter and is named isXXX or getXXX",
 					e);
 			p.removeFromIgnoreList(e);
 		}
 		return Optional.empty();
 	}
 
-	private FieldDescription visiteExecutableGet(ExecutableElement e, String prefix,
+	private Optional<FieldDescription> visiteExecutableGet(ExecutableElement e, String prefix,
 			ProvidesMatchersAnnotatedElementMirror p) {
 		String methodName = e.getSimpleName().toString();
 		String fieldNameDirect = methodName.replaceFirst(prefix, "");
 		String fieldName = fieldNameDirect.substring(0, 1).toLowerCase() + fieldNameDirect.substring(1);
-		String fieldType = parseType(e.getReturnType(), false);
-		if (fieldType != null) {
-			p.removeFromIgnoreList(e);
-			return new FieldDescription(p, methodName + "()", fieldName, fieldType,
-					isInSameRound.test(processingEnv.getTypeUtils().asElement(e.asType())), processingEnv, e,
-					e.getReturnType());
-		}
-		return null;
+		return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, methodName + "()",
+				fieldName, e.getReturnType());
+	}
+
+	public Optional<FieldDescription> createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(
+			Element e, ProvidesMatchersAnnotatedElementMirror p, String methodName, String fieldName,
+			TypeMirror typeMirrorForField) {
+		return removeIfNeededAndThenReturn(typeMirrorForField.accept(extractNameVisitor, false)
+				.map(f -> new FieldDescription(p, methodName, fieldName, f,
+						isInSameRound.test(processingEnv.getTypeUtils().asElement(e.asType())), processingEnv, e,
+						typeMirrorForField)),
+				p);
 	}
 
 	@Override
 	protected Optional<FieldDescription> defaultAction(Element e, ProvidesMatchersAnnotatedElementMirror p) {
 		return Optional.empty();
-	}
-
-	private String parseType(TypeMirror type, boolean asPrimitif) {
-		return type.accept(extractNameVisitor, asPrimitif);
 	}
 
 }
--- src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryAnnotationProcessorTest.java
+++ src/test/java/ch/powerunit/extensions/matchers/factoryprocessor/FactoryAnnotationProcessorTest.java
@@ -2,20 +2,35 @@ package ch.powerunit.extensions.matchers.factoryprocessor;
 
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ElementVisitor;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.JavaFileObject;
 import javax.tools.Diagnostic.Kind;
 
-import org.hamcrest.Matcher;
+import org.hamcrest.Factory;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 
 import ch.powerunit.Rule;
 import ch.powerunit.Test;
@@ -45,11 +60,63 @@ public class FactoryAnnotationProcessorTest implements TestSuite {
 	@Mock
 	private TypeElement factoryTE;
 
+	@Mock
+	private JavaFileObject javaFileObject;
+
+	@Mock
+	private ExecutableElement executableElement;
+
+	@Mock
+	private Element classElement;
+
+	@Mock
+	private TypeMirror classTypeMirror;
+
+	@Mock
+	private Name className;
+
+	@Mock
+	private Name elementName;
+
+	@Mock
+	private TypeMirror returnType;
+
+	@Mock
+	private PackageElement packageElement;
+
+	@Mock
+	private Name packageName;
+
+	@Spy
+	private StringWriter outputStream = new StringWriter();
+
 	private void prepareMock() {
 		when(processingEnv.getMessager()).thenReturn(messageUtils);
 		when(processingEnv.getFiler()).thenReturn(filer);
 		when(processingEnv.getTypeUtils()).thenReturn(typeUtils);
 		when(processingEnv.getElementUtils()).thenReturn(elements);
+		when(elements.getTypeElement("org.hamcrest.Factory")).thenReturn(factoryTE);
+		when(executableElement.getModifiers()).thenReturn(EnumSet.of(Modifier.STATIC, Modifier.PUBLIC));
+		when(executableElement.getKind()).thenReturn(ElementKind.METHOD);
+		when(executableElement.getEnclosingElement()).thenReturn(classElement);
+		when(executableElement.getSimpleName()).thenReturn(elementName);
+		when(executableElement.getReturnType()).thenReturn(returnType);
+		when(executableElement.accept(Mockito.any(), Mockito.any()))
+				.thenAnswer(ip -> ip.getArgumentAt(0, ElementVisitor.class).visitExecutable(executableElement,
+						ip.getArgumentAt(1, Object.class)));
+		when(classElement.asType()).thenReturn(classTypeMirror);
+		when(classElement.getSimpleName()).thenReturn(className);
+		when(elements.getPackageOf(classElement)).thenReturn(packageElement);
+		when(packageElement.getQualifiedName()).thenReturn(packageName);
+
+		when(classTypeMirror.toString()).thenReturn("fqn.sn");
+		when(className.toString()).thenReturn("sn");
+		when(elementName.toString()).thenReturn("method");
+		when(returnType.getKind()).thenReturn(TypeKind.VOID);
+		when(returnType.toString()).thenReturn("void");
+		when(packageName.toString()).thenReturn("fqn");
+		when(elementName.toString()).thenReturn("method");
+
 	}
 
 	@Rule
@@ -83,5 +150,52 @@ public class FactoryAnnotationProcessorTest implements TestSuite {
 		when(processingEnv.getOptions()).thenReturn(Collections.emptyMap());
 		underTest.init(processingEnv);
 		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(false);
+	}
+
+	@Test
+	public void testProcessOneTargetNoAnnotatedElement() throws IOException {
+		when(filer.createSourceFile(Mockito.eq("target"), Mockito.anyVararg())).thenReturn(javaFileObject);
+		when(javaFileObject.openWriter()).thenReturn(outputStream);
+		when(processingEnv.getOptions()).thenReturn(
+				Collections.singletonMap(FactoryAnnotationsProcessor.class.getName() + ".targets", ".*:target"));
+		underTest.init(processingEnv);
+		when(roundEnv.processingOver()).thenReturn(false, true);
+		// First round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+		// Second round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+
+		// Validate
+		String target = outputStream.toString().replace('\r', '\n');
+		assertThat(target).is(containsString("package target"));
+		assertThat(target).is(containsString(
+				"@javax.annotation.Generated(value=\"ch.powerunit.extensions.matchers.factoryprocessor.FactoryAnnotationsProcessor\","));
+		assertThat(target).is(containsString("public interface target {"));
+		assertThat(target).is(containsString("public static final target DSL = new target() {}"));
+	}
+ 
+	@Test
+	public void testProcessOneTargetOneAnnotatedElement() throws IOException {
+		when(filer.createSourceFile(Mockito.eq("target"), Mockito.anyVararg())).thenReturn(javaFileObject);
+		when(javaFileObject.openWriter()).thenReturn(outputStream);
+		when(processingEnv.getOptions()).thenReturn(
+				Collections.singletonMap(FactoryAnnotationsProcessor.class.getName() + ".targets", ".*:target"));
+		underTest.init(processingEnv);
+		when(roundEnv.processingOver()).thenReturn(false, true);
+		when(roundEnv.getElementsAnnotatedWith(Factory.class))
+				.thenReturn((Set) Collections.singleton(executableElement));
+		when(roundEnv.getRootElements()).thenReturn((Set) Collections.singleton(classElement));
+		// First round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+		// Second round
+		assertThat(underTest.process(Collections.emptySet(), roundEnv)).is(true);
+
+		// Validate
+		String target = outputStream.toString().replace('\r', '\n');
+		assertThat(target).is(containsString("package target"));
+		assertThat(target).is(containsString(
+				"@javax.annotation.Generated(value=\"ch.powerunit.extensions.matchers.factoryprocessor.FactoryAnnotationsProcessor\","));
+		assertThat(target).is(containsString("public interface target {"));
+		assertThat(target).is(containsString("public static final target DSL = new target() {}"));
 	}
 }
