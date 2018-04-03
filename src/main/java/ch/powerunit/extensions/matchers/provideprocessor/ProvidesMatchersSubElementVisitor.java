fa63130f00878887a69d978eb76a1075e327306a
Mathieu Boretti <mathieu.boretti@gmail.com>
2018-04-03 19:53:58 +0200

    Remove argument to fix Issue #99 (#100)
    * Remove one parameter to fix issue #99
    
    * Remove import
    
    Close #99

--- src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitor.java
+++ src/main/java/ch/powerunit/extensions/matchers/provideprocessor/ProvidesMatchersSubElementVisitor.java
@@ -27,7 +27,6 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleElementVisitor8;
 import javax.tools.Diagnostic.Kind;
 
@@ -58,7 +57,7 @@ public class ProvidesMatchersSubElementVisitor
 	public Optional<FieldDescription> visitVariable(VariableElement e, ProvidesMatchersAnnotatedElementMirror p) {
 		if (e.getModifiers().contains(Modifier.PUBLIC) && !e.getModifiers().contains(Modifier.STATIC)) {
 			String fieldName = e.getSimpleName().toString();
-			return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, fieldName, fieldName);
+			return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, fieldName);
 		}
 		if (p.isInsideIgnoreList(e)) {
 			processingEnv.getMessager().printMessage(Kind.MANDATORY_WARNING,
@@ -94,15 +93,14 @@ public class ProvidesMatchersSubElementVisitor
 		String methodName = e.getSimpleName().toString();
 		String fieldNameDirect = methodName.replaceFirst(prefix, "");
 		String fieldName = fieldNameDirect.substring(0, 1).toLowerCase() + fieldNameDirect.substring(1);
-		return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, methodName + "()",
-				fieldName);
+		return createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(e, p, fieldName);
 	}
 
 	public Optional<FieldDescription> createFieldDescriptionIfApplicableAndRemoveElementFromListWhenApplicable(
-			Element e, ProvidesMatchersAnnotatedElementMirror p, String methodName, String fieldName) {
+			Element e, ProvidesMatchersAnnotatedElementMirror p, String fieldName) {
 		return removeIfNeededAndThenReturn(
 				((e instanceof ExecutableElement) ? ((ExecutableElement) e).getReturnType() : e.asType())
-						.accept(extractNameVisitor, false).map(f -> new FieldDescription(p, methodName, fieldName, f,
+						.accept(extractNameVisitor, false).map(f -> new FieldDescription(p, fieldName, f,
 								isInSameRound.test(processingEnv.getTypeUtils().asElement(e.asType())), e)),
 				p);
 	}
--- src/test/java/ch/powerunit/extensions/matchers/provideprocessor/FieldDescriptionTest.java
+++ src/test/java/ch/powerunit/extensions/matchers/provideprocessor/FieldDescriptionTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Name;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.PrimitiveType;
 import javax.lang.model.type.TypeKind;
@@ -49,6 +50,9 @@ public class FieldDescriptionTest implements TestSuite {
 	private ExecutableElement executableElement;
 
 	@Mock
+	private Name executableElementName;
+
+	@Mock
 	private ProvidesMatchersAnnotatedElementMirror provideMatchersAnnotatedElementMirror;
 
 	@Mock
@@ -69,6 +73,8 @@ public class FieldDescriptionTest implements TestSuite {
 		when(processingEnv.getElementUtils()).thenReturn(elements);
 		when(processingEnv.getTypeUtils()).thenReturn(types);
 		when(primitiveType.getKind()).thenReturn(TypeKind.BOOLEAN);
+		when(executableElement.getSimpleName()).thenReturn(executableElementName);
+		when(executableElementName.toString()).thenReturn("field");
 		when(executableElement.getReturnType()).thenReturn(primitiveType);
 		when(executableElement.getAnnotationsByType(Mockito.eq(AddToMatcher.class))).thenReturn(new AddToMatcher[] {});
 		when(primitiveType.accept(Mockito.any(TypeVisitor.class), Mockito.any(Object.class)))
@@ -98,48 +104,48 @@ public class FieldDescriptionTest implements TestSuite {
 
 	@Test
 	public void testAsMatcherSafely() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::asMatchesSafely, " ").is(
 				" if(!field.matches(actual)) {\n   mismatchDescription.appendText(\"[\"); field.describeMismatch(actual,mismatchDescription); mismatchDescription.appendText(\"]\\n\");\n   result=false;\n }");
 	}
 
 	@Test
 	public void testAsDescribeTo() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::asDescribeTo, " ")
 				.is(" description.appendText(\"[\").appendDescriptionOf(field).appendText(\"]\\n\");");
 	}
 
 	@Test
 	public void testAsMatchesSafely() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::asMatchesSafely, " ").is(
 				" if(!field.matches(actual)) {\n   mismatchDescription.appendText(\"[\"); field.describeMismatch(actual,mismatchDescription); mismatchDescription.appendText(\"]\\n\");\n   result=false;\n }");
 	}
 
 	@Test
 	public void testGetDslForDefault() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::getDslForDefault, " ").is(
-				" /**\n  * Add a validation on the field `field`.\n  * <p>\n  *\n  * <i>{@link fqn.sn#is This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param matcher a Matcher on the field.\n  * @return the DSL to continue the construction of the matcher.\n  * @see org.hamcrest.Matchers The main class from hamcrest that provides default matchers.\n  */\n null field(org.hamcrest.Matcher<? super boolean> matcher);\n /**\n  * Add a validation on the field `field`.\n  * <p>\n  *\n  * <i>{@link fqn.sn#is This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param value an expected value for the field, which will be compared using the is matcher.\n  * @return the DSL to continue the construction of the matcher.\n  * @see org.hamcrest.Matchers#is(java.lang.Object)\n  */\n default null field(boolean value){\n  return field(org.hamcrest.Matchers.is(value));\n } /**\n  * Add a validation on the field `field` by converting the received field before validat it.\n  * <p>\n  *\n  * <i>{@link fqn.sn#is This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param converter a function to convert the field.\n  * @param matcher a matcher on the resulting.\n  * @param <_TARGETFIELD> The type which this field must be converter.\n  * @return the DSL to continue the construction of the matcher.\n  */\n default <_TARGETFIELD> null fieldAs(java.util.function.Function<boolean,_TARGETFIELD> converter,org.hamcrest.Matcher<? super _TARGETFIELD> matcher){\n  return field(asFeatureMatcher(\" <field is converted> \",converter,matcher));\n }");
+				" /**\n  * Add a validation on the field `field`.\n  * <p>\n  *\n  * <i>{@link fqn.sn#field() This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param matcher a Matcher on the field.\n  * @return the DSL to continue the construction of the matcher.\n  * @see org.hamcrest.Matchers The main class from hamcrest that provides default matchers.\n  */\n null field(org.hamcrest.Matcher<? super boolean> matcher);\n /**\n  * Add a validation on the field `field`.\n  * <p>\n  *\n  * <i>{@link fqn.sn#field() This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param value an expected value for the field, which will be compared using the is matcher.\n  * @return the DSL to continue the construction of the matcher.\n  * @see org.hamcrest.Matchers#is(java.lang.Object)\n  */\n default null field(boolean value){\n  return field(org.hamcrest.Matchers.is(value));\n } /**\n  * Add a validation on the field `field` by converting the received field before validat it.\n  * <p>\n  *\n  * <i>{@link fqn.sn#field() This field is accessed by using this approach}.</i>\n  * <p>\n  * <b>In case method specifing a matcher on a fields are used several times, only the last setted matcher will be used.</b> \n  * When several control must be done on a single field, hamcrest itself provides a way to combine several matchers (See for instance {@link org.hamcrest.Matchers#both(org.hamcrest.Matcher)}.\n  *\n  * @param converter a function to convert the field.\n  * @param matcher a matcher on the resulting.\n  * @param <_TARGETFIELD> The type which this field must be converter.\n  * @return the DSL to continue the construction of the matcher.\n  */\n default <_TARGETFIELD> null fieldAs(java.util.function.Function<boolean,_TARGETFIELD> converter,org.hamcrest.Matcher<? super _TARGETFIELD> matcher){\n  return field(asFeatureMatcher(\" <field is converted> \",converter,matcher));\n }");
 	}
 
 	@Test
 	public void testGetMatcherForField() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::getMatcherForField, " ").is(
-				" private static class FieldMatcher extends org.hamcrest.FeatureMatcher<fqn.sn,boolean> {\n   public FieldMatcher(org.hamcrest.Matcher<? super boolean> matcher) {\n     super(matcher,\"field\",\"field\");\n   }\n   protected boolean featureValueOf(fqn.sn actual) {\n     return actual.is;\n   }\n }\n");
+				" private static class FieldMatcher extends org.hamcrest.FeatureMatcher<fqn.sn,boolean> {\n   public FieldMatcher(org.hamcrest.Matcher<? super boolean> matcher) {\n     super(matcher,\"field\",\"field\");\n   }\n   protected boolean featureValueOf(fqn.sn actual) {\n     return actual.field();\n   }\n }\n");
 	}
 
 	@Test
 	public void testGetSupplierMatcher() {
-		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "is", "field",
-				"boolean", false, executableElement);
+		FieldDescription undertest = new FieldDescription(provideMatchersAnnotatedElementMirror, "field", "boolean",
+				false, executableElement);
 		assertThatFunction(undertest::getSupplierMatcher, " ").is(
 				" private static class FieldMatcherSupplier extends org.hamcrest.FeatureMatcher<java.util.function.Supplier<>,> {\n   public FieldMatcherSupplier(org.hamcrest.Matcher<? super > matcher) {\n     super(matcher,\"with supplier result\",\"with supplier result\");\n   }\n   protected  featureValueOf(java.util.function.Supplier<> actual) {\n     return actual.get();\n   }\n }\n");
 	}
