49828dd46110d6d2cbb2b1283b29418c99727e06
Mathieu Boretti <mathieu.boretti@gmail.com>
2020-05-19 21:22:08 +0200

    Issues #289 - Add support for chaining with Matcher in same round

--- src/test/java/ch/powerunit/extensions/matchers/samples/MapSample.java
+++ src/test/java/ch/powerunit/extensions/matchers/samples/MapSample.java
@@ -3,14 +3,20 @@ package ch.powerunit.extensions.matchers.samples;
 import java.util.Map;
 
 import ch.powerunit.extensions.matchers.ProvideMatchers;
+import ch.powerunit.extensions.matchers.samples.extension.MyTestWithoutGeneric;
+import ch.powerunit.extensions.matchers.samples.extension.MyTestWithoutGeneric2;
 
 @ProvideMatchers
 public class MapSample<E, V> {
 	public Map<String, Integer> map1;
 
 	public Map<E, V> map2;
-	
+
 	public Map map3;
-	
-	public Map<?,?> map4;
+
+	public Map<?, ?> map4;
+
+	public Map<String, MyTestWithoutGeneric> map5;
+
+	public Map<String, MyTestWithoutGeneric2> map6;
 }
